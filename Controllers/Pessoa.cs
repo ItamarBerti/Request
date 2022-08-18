ad1b08b63f4a0f643f6188d8bb3203edf52de1e9
guest01 <guest01@ITELABD17>
2022-08-17 22:34:38 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/Pessoa.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiAula.Controllers
+{
+   public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public int Idade { get; set; }
+        public string Id { get; set; }
+    }
+}
---
+++ Controllers/PessoaController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiAula.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]    
+    public class PessoaController : ControllerBase
+    {
+       private static readonly List<Pessoa> pessoas = new List<Pessoa>();
+
+        [HttpPost]
+        public IActionResult Salvar(Pessoa pessoa)
+        {
+            pessoas.Add (pessoa);
+
+            return Ok (pessoa);
+        }
+        [HttpPut]
+        public IActionResult Atualizar(Pessoa pessoa)
+        {
+            var res = pessoas.FirstOrDefault(p => p.Nome == pessoa.Nome);         
+            if (res != null)
+            {
+                res.Nome = pessoa.Nome ;
+                res.Cpf = pessoa.Cpf;
+                res.Idade = pessoa.Idade;
+            }          
+            else
+            {
+                return NotFound();
+            }
+            return Ok (pessoa);            
+        }
+        [HttpGet]
+        public IActionResult BuscarTodos()
+        {
+            return Ok(pessoas);
+        }
+        [HttpDelete]
+        public IActionResult Deletar(Pessoa pessoa)
+        {
+            Pessoa x = pessoas.Find(p => p.Nome == pessoa.Nome);
+            if (x != null)
+            {
+                pessoas.Remove(x);
+
+                return Ok("true");
+            }
+            return NotFound("erro ao tentar deletar");                                    
+        }
+    }
+}
